31c9abfd76a799400e5a15afa49e732e97a0c724
TOOMdaMoa <mailtotoom@gmail.com>
2020-01-01 20:55:22 +0100

    data updating

--- wta_scraper.py
+++ wta_scraper.py
@@ -1,8 +1,37 @@
+import datetime
+import os
+from pathlib import Path
+from shutil import copy2
+
+import numpy as np
 import pandas as pd
 import requests
 
+ERROR_STRING = -12513911  # random value to get rid of NaNs
+
+
+def different_dataframe(data_year: pd.DataFrame, filepath: str) -> bool:
+    data_year = data_year.replace(np.nan, ERROR_STRING)
+    last_data = pd.read_excel(filepath, keep_default_na=False)
+    last_data = last_data.drop(['Unnamed: 0'], axis=1)
+    last_data = last_data.replace(r'^\s*$', ERROR_STRING, regex=True)
+    dtypes = data_year.dtypes
+    last_data = last_data.astype(dtypes)
+    last_data = last_data.set_index([pd.Index(range(1, len(data_year) + 1))])
+    return (last_data != data_year).any().any()
+
 
 def scrape_wta():
+    f = open("data/last_run.txt", "r")
+    last_run_date = f.read()
+    f.close()
+    last_run_date_path = Path(f"data/{last_run_date}")
+
+    today = datetime.datetime.now().date()
+    f = open("data/last_run.txt", "w")
+    f.write(str(today))
+    f.close()
+
     year = 2019
     while True:
         data_year = pd.DataFrame()
@@ -20,7 +49,17 @@ def scrape_wta():
             page = page + 1
 
         if not data_year.empty:
-            data_year.to_excel(f"data/wta_{year}.xlsx")
+            data_year = data_year.set_index([pd.Index(range(1, len(data_year) + 1))])
+            data_year.tourn_year = data_year.tourn_year.astype(int)
+            data_year.PlayerNbr = data_year.PlayerNbr.astype(int)
+            filepath = f"data/wta_{year}.xlsx"
+            if not os.path.exists(filepath):
+                data_year.to_excel(filepath)
+            elif different_dataframe(data_year, filepath):
+                print(f"Changes in year {year}")
+                last_run_date_path.mkdir(parents=True, exist_ok=True)
+                copy2(filepath, last_run_date_path)
+                data_year.to_excel(filepath)
         else:
             break
         year = year - 1
