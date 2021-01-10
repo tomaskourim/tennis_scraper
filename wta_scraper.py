7e4d938dec7ff6ffed4fc3b34fb08c973db06670
Tomáš Kouřim <kourim@outlook.com>
2021-01-10 23:22:55 +0100

    Minor fixes

--- wta_scraper.py
+++ wta_scraper.py
@@ -9,8 +9,7 @@ import numpy as np
 import pandas as pd
 import requests
 
-ERROR_STRING = -12513911  # random value to get rid of NaNs
-STARTING_YEAR = 2020
+ERROR_STRING = -12513911  # random constant to get rid of NaNs
 
 
 def different_dataframe(data_year: pd.DataFrame, filepath: str) -> bool:
@@ -30,19 +29,20 @@ def different_dataframe(data_year: pd.DataFrame, filepath: str) -> bool:
     return (last_data != data_year).any().any() if len(last_data) == len(data_year) else True
 
 
-def scrape_wta(current_dir: str):
-    last_run_path = os.path.join(current_dir, "last_run.txt")
+def scrape_wta(working_dir: str):
+    last_run_path = os.path.join(working_dir, "last_run.txt")
     f = open(last_run_path, "r")
     last_run_date = f.read()
     f.close()
-    last_run_date_path = Path(os.path.join(current_dir, f"{last_run_date}"))
+    last_run_date_path = Path(os.path.join(working_dir, f"{last_run_date}"))
 
     today = datetime.datetime.now().date()
     f = open(last_run_path, "w")
     f.write(str(today))
     f.close()
 
-    year = STARTING_YEAR  # get it automatically, but it does not work on January 1st
+    year = today.year  # get it automatically, but it does not work on January 1st
+    original_year = year
     while True:
         data_year = pd.DataFrame()
         page = 0
@@ -53,8 +53,12 @@ def scrape_wta(current_dir: str):
             # extracting data in json format
             data = r.json()
             if len(data) == 0:
-                print(year, page)
-                break
+                if year == original_year:
+                    print(year, page)
+                    year = year - 1
+                else:
+                    print(year, page)
+                    break
             data_year = data_year.append(pd.DataFrame(data))
             page = page + 1
 
@@ -62,7 +66,7 @@ def scrape_wta(current_dir: str):
             data_year = data_year.set_index([pd.Index(range(1, len(data_year) + 1))])
             data_year.tourn_year = data_year.tourn_year.astype(int)
             data_year.PlayerNbr = data_year.PlayerNbr.astype(int)
-            filepath = os.path.join(current_dir, f"wta_{year}.xlsx")
+            filepath = os.path.join(working_dir, f"wta_{year}.xlsx")
             if not os.path.exists(filepath):
                 data_year.to_excel(filepath)
             elif different_dataframe(data_year, filepath):
